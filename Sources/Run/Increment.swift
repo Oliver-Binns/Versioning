bbb520038256c09bbc0ce971a699b10805ca6600
Oliver BINNS <mail@oliverbinns.co.uk>
2023-11-06 22:05:21 +0000

    ci: implemented steps for version incrementing

--- Sources/Run/Increment.swift
+++ Sources/Run/Increment.swift
@@ -2,8 +2,20 @@ import ArgumentParser
 import Versioning
 
 struct Increment: ParsableCommand {
-    mutating func run() throws {
-        print("hi")
+    @Option(name: .shortAndLong, help: "The initial version to be incremented")
+    private var initialVersion: String
+    
+    //@Option(name: .shortAndLong, help: "A commit message to validate")
+    //private var messages: [String]
+    
+    mutating func run() throws -> String {
+        /*try messages
+            .map(Commit.init)
+            .compactMap(\.versionIncrement)
+            .reduce(initialVersion) { version, increment in
+                version.apply(increment: increment)
+            }*/
+        Version.one.description
     }
     
     
