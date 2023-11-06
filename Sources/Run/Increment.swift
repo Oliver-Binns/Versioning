10e01bc6585cd88798d451f4c868f4d24ed7b738
Oliver BINNS <mail@oliverbinns.co.uk>
2023-11-06 23:11:41 +0000

    ci: implemented output for Swift Package step

--- Sources/Run/Increment.swift
+++ Sources/Run/Increment.swift
@@ -3,28 +3,18 @@ import Versioning
 
 struct Increment: ParsableCommand {
     @Option(name: .shortAndLong, help: "The initial version to be incremented")
-    private var initialVersion: String
+    private var initialVersion: Version
     
-    //@Option(name: .shortAndLong, help: "A commit message to validate")
-    //private var messages: [String]
+    @Option(name: .shortAndLong, help: "A commit message to validate")
+    private var messages: [String]
     
-    mutating func run() throws -> String {
-        /*try messages
+    mutating func run() throws {
+        let version = try messages
             .map(Commit.init)
             .compactMap(\.versionIncrement)
             .reduce(initialVersion) { version, increment in
                 version.apply(increment: increment)
-            }*/
-        Version.one.description
-    }
-    
-    
-    static func calculateNewVersion(initialVersion: Version,
-                                    commits: [Commit]) -> Version {
-        commits
-            .compactMap(\.versionIncrement)
-            .reduce(initialVersion) { version, increment in
-                version.apply(increment: increment)
             }
+        print(version)
     }
 }
