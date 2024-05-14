95a4286ac65db4da64cb783226875a207d0a9de9
Oliver Binns <mail@oliverbinns.co.uk>
2024-05-14 09:04:38 +0100

    feat: allow consumers of the GitHub Action to pass in the contains breaking change parameter to the Validate action

--- Sources/Run/Validate.swift
+++ Sources/Run/Validate.swift
@@ -5,12 +5,12 @@ struct Validate: ParsableCommand {
     @Option(name: .shortAndLong, help: "A commit message to validate")
     private var message: String
     
-    @Option(name: .shortAndLong, help: "Require breaking change")
-    private var requireBreakingChange: Bool = false
+    @Option(name: .shortAndLong, help: "Whether the commit contains a breaking change in the public facing API")
+    private var containsBreakingChange: Bool = false
     
     mutating func run() throws {
         let commit = try Commit(string: message)
-        if requireBreakingChange && !commit.isBreakingChange {
+        if containsBreakingChange && !commit.isBreakingChange {
             throw ValidationError.breakingChangeNotFlagged
         }
         print("Validated commit message (\(message)) successfully")
