13dd7f4d0504f334dd378e6b56deea5303cb1f1a
Oliver Binns <mail@oliverbinns.co.uk>
2024-05-14 07:59:23 +0100

    ci: attempt enforcing breaking changes

--- Sources/Run/Validate.swift
+++ Sources/Run/Validate.swift
@@ -5,8 +5,14 @@ struct Validate: ParsableCommand {
     @Option(name: .shortAndLong, help: "A commit message to validate")
     private var message: String
     
+    @Option(name: .shortAndLong, help: "Require breaking change")
+    private var requireBreakingChange: Bool = false
+    
     mutating func run() throws {
-        _ = try Commit(string: message)
+        let commit = try Commit(string: message)
+        if requireBreakingChange && !commit.isBreakingChange {
+            throw ValidationError.breakingChangeNotFlagged
+        }
         print("Validated commit message (\(message)) successfully")
     }
 }
