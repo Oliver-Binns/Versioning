80323cfb4120bdfa36f323d488745aca9c76f8dd
Oliver Binns <mail@oliverbinns.co.uk>
2025-02-25 16:00:55 +0000

    build: update to Swift 6

--- Sources/Versioning/Commit.swift
+++ Sources/Versioning/Commit.swift
@@ -30,27 +30,6 @@ public struct Commit {
             self.isBreakingChange = match.output.breaking != nil
             self.message = String(match.output.message)
         }
-
-
-        /*let components = string.split(separator: ": ", maxSplits: 1)
-        guard components.count == 2 else {
-            throw CommitFormatError.invalid(string)
-        }
-
-        self.scope = nil
-        self.isBreakingChange = components[0].last == "!"
-        
-        if isBreakingChange {
-            let messageLength = components[0].count
-            let trimmedExclamation = components[0].prefix(messageLength-1)
-            self.type = CommitType(rawValue: String(trimmedExclamation))!
-        } else if let type = CommitType(rawValue: String(components[0])) {
-            self.type = type
-        } else {
-            throw CommitFormatError.invalidPrefix(components[0])
-        }
-        
-        message = String(components[1])*/
     }
 }
 
