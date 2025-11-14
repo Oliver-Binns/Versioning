361089d8998fc772fdb54e331fb2cb1d2ff9bb86
Oliver Binns <mail@oliverbinns.co.uk>
2025-11-14 16:59:14 +0000

    feat: breaking change detection (#14)
    * ci: added action step to detect breaking changes
    
    * ci: attempt to fix syntax error in script
    
    * ci: fetch tags to allow branch comparison
    
    * ci: fetch base ref branch
    
    * test: added a breaking change to test ci behaviour
    
    * ci: attempt enforcing breaking changes
    
    * test: remove breaking change
    
    * ci: fix logic if no breaking change
    
    * test: attempt run with breaking change
    
    * chore: remove breaking change
    
    * feat: allow consumers of the GitHub Action to pass in the contains breaking change parameter to the Validate action
    
    * docs: add details of contains breaking change to the README.md
    
    * test: added unit tests for breaking change management
    
    * chore: silence warning
    
    * fix: build issue

--- Sources/Run/Validate.swift
+++ Sources/Run/Validate.swift
@@ -5,8 +5,14 @@ struct Validate: ParsableCommand {
     @Option(name: .shortAndLong, help: "A commit message to validate")
     private var message: String
     
+    @Option(name: .shortAndLong, help: "Whether the commit contains a breaking change in the public facing API")
+    private var containsBreakingChange: Bool = false
+    
     mutating func run() throws {
-        _ = try Commit(string: message)
+        let commit = try Commit(string: message)
+        if containsBreakingChange && !commit.isBreakingChange {
+            throw ValidationError.breakingChangeNotFlagged
+        }
         print("Validated commit message (\(message)) successfully")
     }
 }
