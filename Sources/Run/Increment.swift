cb1149dfe753ecfeb47f84af6d5a1e48828e97f2
Ben McKillop <benmckillop@icloud.com>
2025-11-19 19:24:33 +0000

    fix: changing short and long

--- Sources/Run/Increment.swift
+++ Sources/Run/Increment.swift
@@ -15,7 +15,7 @@ struct Increment: AsyncParsableCommand {
     @Option(name: .long, help: "True if a Git tag should be created but not a GitHub release")
     private var tagOnly: Bool = false
 
-    @Option(name: .shortAndLong, help: "A suffix to be added to the version string")
+    @Option(name: .long, help: "A suffix to be added to the version string")
     private var suffix: String?
 
     @Flag(name: .shortAndLong)
